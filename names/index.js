5450fce035d9c74f2be6c7beeaabe40bcf3af16d
rajanala17 <rleelasaipavan@gmail.com>
2024-03-16 09:57:31 +0000

    solution

---
+++ names/index.js
@@ -0,0 +1,6 @@
+const people = require('../country/state/city/index')
+const getFirstNames = require('../utilities/utils/index')
+const getPeopleInCity = (people){
+    return getFirstNames(people)
+}
+module.exports = getPeopleInCity;
